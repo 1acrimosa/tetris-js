b27b8a0a4c51e289bac55cdda25a982288e6006b
1acrimosa <113746184+1acrimosa@users.noreply.github.com>
2023-07-27 17:34:48 +0500

    add: draw methods

--- tetris.js
+++ tetris.js
@@ -84,7 +84,119 @@ let gameLoop = () => {
 
 let update = () => {}
 
-let draw = () => {}
+let drawRect = (x, y, width, height, color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+};
+
+let drawBackground = () => {
+  drawRect(0, 0, canvas.width, canvas.height, "#bca0dc");
+  for (let i = 0; i < squareCountX + 1; i++) {
+    drawRect(
+      size * i - whiteLineThickness,
+      0,
+      whiteLineThickness,
+      canvas.height,
+      "white"
+    );
+  }
+
+  for (let i = 0; i < squareCountY + 1; i++) {
+    drawRect(
+      0,
+      size * i - whiteLineThickness,
+      canvas.width,
+      whiteLineThickness,
+      "white"
+    );
+  }
+};
+
+let drawCurrentTetris = () => {
+  for (let i = 0; i < currentShape.template.length; i++) {
+    for (let j = 0; j < currentShape.template.length; j++) {
+      if (currentShape.template[i][j] == 0) continue;
+      ctx.drawImage(
+        image,
+        currentShape.imageX,
+        currentShape.imageY,
+        imageSquareSize,
+        imageSquareSize,
+        Math.trunc(currentShape.x) * size + size * i,
+        Math.trunc(currentShape.y) * size + size * j,
+        size,
+        size
+      );
+    }
+  }
+};
+
+let drawSquares = () => {
+  for (let i = 0; i < gameMap.length; i++) {
+    let t = gameMap[i];
+    for (let j = 0; j < t.length; j++) {
+      if (t[j].imageX == -1) continue;
+      ctx.drawImage(
+        image,
+        t[j].imageX,
+        t[j].imageY,
+        imageSquareSize,
+        imageSquareSize,
+        j * size,
+        i * size,
+        size,
+        size
+      );
+    }
+  }
+};
+
+let drawNextShape = () => {
+  nctx.fillStyle = "#bca0dc";
+  nctx.fillRect(0, 0, nextShapeCanvas.width, nextShapeCanvas.height);
+  for (let i = 0; i < nextShape.template.length; i++) {
+    for (let j = 0; j < nextShape.template.length; j++) {
+      if (nextShape.template[i][j] == 0) continue;
+      nctx.drawImage(
+        image,
+        nextShape.imageX,
+        nextShape.imageY,
+        imageSquareSize,
+        imageSquareSize,
+        size * i,
+        size * j + size,
+        size,
+        size
+      );
+    }
+  }
+};
+
+let drawScore = () => {
+  sctx.clearRect(0, 0, scoreCanvas.width, scoreCanvas.height);
+  sctx.font = "64px Poppins";
+  sctx.fillStyle = "black";
+  sctx.fillText(score, 10, 50);
+};
+
+let drawGameOver = () => {
+  ctx.font = "64px Poppins";
+  ctx.fillStyle = "black";
+  ctx.fillText("Game Over!", 10, canvas.height / 2);
+};
+
+
+let draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
+    drawSquares();
+    drawCurrentTetris();
+    drawNextShape();
+    drawScore();
+    if (gameOver) {
+        drawGameOver();
+    }
+};
 
 let getRandomShape = () => {
   return Object.create(shapes[Math.floor(Math.random() * shapes.length)]);
